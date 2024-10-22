47eab8da03bcfb2bde1a8e6ce91a0f50eeb770ac
OVECJOE <vohachor@gmail.com>
2024-10-22 17:58:46 +0100

    [WIP]: implementation of accounts service: avatar generation in progress

--- app/utils/shared.py
+++ app/utils/shared.py
@@ -2,6 +2,9 @@ from bson import ObjectId
 from pymongo.database import Database
 import hashlib
 import random
+from PIL import Image, ImageDraw, ImageFont
+import os
+from io import BytesIO
 
 from connectors import MongoDBConnector
 from validators import validate_db_connection
@@ -61,3 +64,74 @@ class UsernameGenerator:
 
     def __str__(self):
         return f'{self.__prefix}_{self.__assignable_number}'
+
+
+"""
+    This class is used to generate avatars for users. The avatars are generated using the user's username.
+    The class generates a random background color and a text with the first letter of the username.
+
+    Adapted from: https://github.com/maethor/avatar-generator/blob/b6cce9a1d3eac770555ed944d3d1caa48424bed4/avatar_generator/__init__.py
+"""
+class AvatarGenerator:
+    FONT_COLOR = (255, 255, 255)
+    MIN_RENDER_SIZE = 512
+
+    @staticmethod
+    def __background_color(s: int) -> tuple:
+        random.seed(s)
+        r = g = b = 255
+        while r + g + b > 255*2:
+            r = random.randint(0, 255)
+            g = random.randint(0, 255)
+            b = random.randint(0, 255)
+
+        return r, g, b
+    
+    @staticmethod
+    def __font(size: int) -> ImageFont.FreeTypeFont:
+        path = os.path.join(os.path.dirname(__file__), 'fonts', 'Roboto-Regular.ttf')
+        return ImageFont.truetype(path, size=int(0.8 * size))
+    
+    @staticmethod
+    def __text(text: str) -> str:
+        return text[0].upper() if text else '#'
+    
+    @staticmethod
+    def __text_position(size: int, text: str, font: ImageFont.FreeTypeFont) -> tuple:
+        left, top, right, bottom = font.getbbox(text)
+
+        width = right - left
+        left = (size - width) / 2.0
+
+        height = bottom - top
+        top = (size - height) / 5.5
+
+        return left, top
+    
+    @classmethod
+    def generate(cls, size: int, string: str, filetype='PNG') -> bytes:
+        render_size = max(size, cls.MIN_RENDER_SIZE)
+        image = Image.new('RGB', (render_size, render_size), cls.__background_color(string))
+        draw = ImageDraw.Draw(image)
+
+        font = cls.__font(size)
+        text = cls.__text(string)
+        draw.text(
+            cls.__text_position(render_size, text, font),
+            text,
+            fill=cls.FONT_COLOR,
+            font=font
+        )
+
+        stream = BytesIO()
+        image = image.resize((size, size), Image.Resampling.LANCZOS)
+        image.save(stream, format=filetype, optimize=True)
+        return stream.getvalue()
+    
+    def random(self):
+        return self.generate(
+            random.randint(128, 512),
+            UsernameGenerator(),
+            random.choice(['PNG', 'JPEG'])
+        )
+
